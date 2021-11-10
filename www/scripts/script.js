3349aa1f763207e248c93bae0cc4b32635be5418
Алексей Гучков <87633877+AGuchkov@users.noreply.github.com>
2021-11-10 17:37:59 +0300

    Add adptive

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,9 @@
 console.log('script work!');
+const burger = document.querySelector('.burger');
+const burgerWrap = document.querySelector('.burger-wrap');
+const burgerInner = document.querySelector('.burger-inner');
+
+burger.addEventListener("click", function (e) {
+  burgerWrap.classList.toggle('burger-wrap-active');
+
+});
