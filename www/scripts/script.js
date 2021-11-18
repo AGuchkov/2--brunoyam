55bcfdea5b7018e4bbf401f873b1640d593f4853
Алексей Гучков <87633877+AGuchkov@users.noreply.github.com>
2021-11-18 17:28:36 +0300

    Add files via upload

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,18 +1,64 @@
-console.log('script work!');
-const burger = document.querySelector('.burger');
-const burgerWrap = document.querySelector('.burger-wrap');
-const burgerInner = document.querySelectorAll('.burger-inner');
+$(document).ready(function () {
 
-const mainNav = document.querySelector('.main-nav');
+  $('.burger').on('click', function () {
 
-let example = function (a) {
-  a.classList.toggle('burger-inner-active')
-};
+    $('.main-nav').slideToggle();
+    $('.burger-wrap').toggleClass('burger-wrap-active');
+    $('.burger-inner').toggleClass('burger-inner-active');
+  });
 
-burger.addEventListener("click", function (e) {
-  burgerWrap.classList.toggle('burger-wrap-active');
-  for (i = 0; i < burgerInner.length; i++) {
-    example(burgerInner[i]);
-  }
-  mainNav.classList.toggle('main-nav-active');
+
+  // Табы в контактах
+  $('.contacts-tab-link').on('click', function (e) {
+    e.preventDefault();
+
+    let index = $(this).index('.contacts-tab-link');
+
+    $('.contacts-tab-link').removeClass('active');
+    $(this).addClass('active');
+
+    $('.contacts-content').removeClass('active');
+    $('.contacts-content').eq(index).addClass('active');
+  });
+
+  // Фильтр в портфолио
+  $('.filter-link').on('click', function (e) {
+    e.preventDefault();
+
+    let linkType = $(this).data('type');
+
+    $('.filter-link').removeClass('active');
+    $(this).addClass('active');
+
+    if (linkType === 'all') {
+      $('.cat-item').show();
+      return;
+    }
+
+    $('.cat-item').each(function () {
+      let portfolioType = $(this).data('type');
+
+      if (linkType === portfolioType) {
+        $(this).show();
+        return;
+      }
+
+      $(this).hide();
+    });
+  });
+
+  // Кнопка "Все бабочки" в каталоге
+  $('.all').on('click', function (e) {
+    e.preventDefault();
+
+    $('.cat-item').show();
+    return;
+
+  });
+
+
+  $('.plus').on('click', function (e) {
+
+    
+  })
 });
