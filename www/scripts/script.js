d4be5c569afbff3800195900be6ca639a213f6da
Алексей Гучков <87633877+AGuchkov@users.noreply.github.com>
2021-11-24 17:23:56 +0300

    Add files via upload

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $('.burger').on('click', function () {
 
     $('.main-nav').slideToggle();
-    $('.burger-wrap').toggleClass('burger-wrap-active');
+    $('.burger').toggleClass('burger-active');
     $('.burger-inner').toggleClass('burger-inner-active');
   });
 
@@ -26,7 +26,7 @@ $(document).ready(function () {
     e.preventDefault();
 
     let linkType = $(this).data('type');
-
+    $('.show-more').data('type', linkType);
     $('.filter-link').removeClass('active');
     $(this).addClass('active');
 
@@ -48,17 +48,91 @@ $(document).ready(function () {
   });
 
   // Кнопка "Все бабочки" в каталоге
-  $('.all').on('click', function (e) {
-    e.preventDefault();
+  // $('.all').on('click', function (e) {
+  //   e.preventDefault();
 
-    $('.cat-item').show();
-    return;
+  //   $('.cat-item').show();
+  //   return;
+
+  // });
+
+
+  // Аккордион
+  let prefIndex;
+
+  $('.accordion-start').on('click', function (e) {
+
+    let currentIndex = $(this).index('.accordion-start');
+
+    if (currentIndex === prefIndex) {
+      $(this).next().slideToggle();
+      $(this).toggleClass('open');
+      $(this).find('.icon-plus').toggleClass('open');
+      return;
+    }
+
+    $(this).next().slideDown();
+    $(this).addClass('open');
+    $(this).find('.icon-plus').addClass('open');
+    $('.accordion-start').eq(prefIndex).next().slideUp();
+    $('.accordion-start').eq(prefIndex).removeClass('open');
+    $('.accordion-start').eq(prefIndex).find('.icon-plus').removeClass('open');
+    prefIndex = currentIndex;
 
   });
 
 
-  $('.plus').on('click', function (e) {
+  //Слайдер slick
 
-    
-  })
+  if ($('.slider').length) {
+    $('.slider').slick({
+    });
+  };
+
+
+  //Ajax подгрузка товаров
+  $('.show-more').on('click', function (e) {
+    e.preventDefault();
+    let dataType = $(this).data('type');
+
+    $.ajax({
+      type: 'POST',
+      url: '../json/cat.json',
+      data: `dataType=${dataType}`,
+      beforeSend: function () {
+        $('.show-more').addClass('load');
+      },
+      success: function (resData) {
+        let html = generateHtml(resData.cat);
+        addToPage(html);
+        $('.show-more').removeClass('load');
+      },
+      error: function () {
+        console.log('Что-то пошло не так!');
+        $('.show-more').removeClass('load');
+      }
+    });
+  });
+
+  function generateHtml(dataArray) {
+    let htmlString = '';
+
+    dataArray.forEach(function (itemArray) {
+      htmlString = htmlString + `<div class="cat-item" data-type="${itemArray.dataType}">
+        <div class="cat-item-wrap">
+          <div class="cat-item-inner">
+            <a href="#" class="cat-item-bg" style="background-image: url('${itemArray.bgUrl}')">
+              <p class="cat-item-text">${itemArray.catItemText}</p>
+            </a>
+          </div>
+        </div>
+      </div>`
+    });
+
+    return htmlString;
+  };
+
+  function addToPage(htmlString) {
+    $('.cat-list').append(htmlString);
+  };
 });
